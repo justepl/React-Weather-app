cf80155ed1a57860d995e87b4c15ee6222f0534e
Franck-Fernandez-pro <franck.fernandez@gmail.com>
2019-03-05 13:57:21 +0100

    First form

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1 className="display-4">Welcome to weather app</h1>
+
+          <form>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Email address
+                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        
       </div>
-    );
+    )
   }
 }
-
-export default App;

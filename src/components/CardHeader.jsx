0e8acb4f991a366dd82f0210e78759b5c851cf90
Franck Fernandez <franck.fernandez@gmail.com>
2019-04-07 22:43:05 +0200

    Refactor CardHeader to component

---
+++ src/components/CardHeader.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import sunSvg from '../assets/svg/sun-solid.svg'
+
+const CardHeader = (props) => {
+  return (
+    <Fragment>
+      <img src={sunSvg} className="weather-card-img-top" alt="sun-logo" />
+      <h5 className="card-title">City - 15°</h5>
+    </Fragment>
+  )
+}
+export default CardHeader
+
--- src/containers/WeatherCard.jsx
+++ src/containers/WeatherCard.jsx
@@ -1,17 +1,18 @@
-import React, { Component, Fragment } from 'react';
-import sunSvg from '../assets/svg/sun-solid.svg'
+import React, { Component } from 'react';
 import tempSvg from '../assets/svg/thermometer-three-quarters-solid.svg'
 import tintSvg from '../assets/svg/tint-solid.svg'
 import locationSvg from '../assets/svg/location-arrow-solid.svg'
+import CardHeader from '../components/CardHeader';
 
 export default class WeatherCard extends Component {
   render() { 
     return (
       <div className="row">
         <div className="card weather-card col-md-6">
-          <img src={sunSvg} className="weather-card-img-top" alt="sun-logo" />
+
+          <CardHeader />
+          
           <div className="card-body">
-            <h5 className="card-title">City - 15°</h5>
             <ul className="list-group list-group-horizontal weather-list-group">
               <li className="list-group-item col">
                 <img src={tempSvg} className="card-item-icon" alt="" /> Temp.
